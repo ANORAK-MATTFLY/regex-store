ee9fd68af8d0dadeae85476acaab1b9dc692a8ee
Ben Matanda <benmatanda354@gmail.com>
2021-09-23 21:39:49 +0100

    feature: more password checks

--- interfaces/credentials.ts
+++
@@ -1,4 +0,0 @@
-export interface credentials {
-    Is_Email(email: string): boolean;
-    Is_Password_with_Eight_Characters_And_At_Least_One_Letter_And_One_Number(password: string): boolean;
-}
--- logic/crudentials.ts
+++ logic/crudentials.ts
@@ -1,4 +1,4 @@
-import { credentials } from "../interfaces/credentials";
+import { credentials } from "../interfaces/credentials_interface";
 
 export class CredentialsValidation implements credentials {
     Is_Email(email: string): boolean {
@@ -7,9 +7,28 @@ export class CredentialsValidation implements credentials {
         return validationCheck;
     }
 
-    Is_Password_with_Eight_Characters_And_At_Least_One_Letter_And_One_Number(password: string): boolean {
+    Is_Password_with_Minimum_Eight_Characters_And_At_Least_One_Letter_And_One_Number(password: string): boolean {
         const pattern: RegExp = new RegExp("^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$");
         const Password_Match_Pattern: boolean = pattern.test(password);
         return Password_Match_Pattern;
     }
+
+    Is_Password_with_At_Least_Eight_Characters_A_Letter_A_Number_And_A_Special_Character(password: string): boolean{
+        const pattern: RegExp = new RegExp("^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$");
+        const Password_Match_Pattern: boolean = pattern.test(password);
+        return Password_Match_Pattern;
+    }
+
+    Is_Password_with_At_Least_Eight_Characters_An_Uppercase_Letter_A_Lowercase_Letter_One_Number(password: string): boolean{
+        const pattern: RegExp = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$");
+        const Password_Match_Pattern: boolean = pattern.test(password);
+        return Password_Match_Pattern;
+    }
+
+    Is_Password_with_At_Least_Eight_Characters_An_Uppercase_Letter_A_Lowercase_Letter_One_Number_A_Special_Character(password: string): boolean{
+        const pattern: RegExp = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$");
+        const Password_Match_Pattern: boolean = pattern.test(password);
+        return Password_Match_Pattern;
+    }
+
 }
