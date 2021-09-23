61feec9e441fd6b5a2639e3a3a8066b8dc52ed5a
Ben Matanda <benmatanda354@gmail.com>
2021-09-23 19:22:58 +0100

    feature: email validation

---
+++ interfaces/credentials.ts
@@ -0,0 +1,3 @@
+export interface credentials {
+    isEmail(email: string): boolean;
+}
---
+++ logic/crudentials.ts
@@ -0,0 +1,8 @@
+import { credentials } from "../interfaces/credentials";
+
+export class CredentialsValidation implements credentials {
+    isEmail(email: string): boolean {
+        const pattern = new RegExp("^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$");
+        return pattern.test(email);
+    }
+}
