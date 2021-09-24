313c89064a59111024bb189a6d7b8be9978146ae
Ben Matanda <benmatanda354@gmail.com>
2021-09-24 10:11:31 +0100

    refactore: patterns

--- logic/credentials.ts
+++ logic/credentials.ts
@@ -6,7 +6,7 @@ export class CredentialsValidation implements credentials {
      * @param {String} string - String on which to perform the validation.
      */
     Is_Name(name: string): boolean {
-        const pattern = new RegExp("^[a-zA-Z ]{2,30}$");
+        const pattern = new RegExp(/^[a-zA-Z ]{2,30}$/);
         const Name_Matches_Pattern = pattern.test(name);
         return Name_Matches_Pattern;
     }
@@ -15,7 +15,7 @@ export class CredentialsValidation implements credentials {
      * @param {String} string - String on which to perform the validation.
      */
     Is_Email(email: string): boolean {
-        const pattern = new RegExp("^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$");
+        const pattern = new RegExp(/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/);
         const Email_Matches_Pattern = pattern.test(email);
         return Email_Matches_Pattern;
     }
@@ -25,7 +25,7 @@ export class CredentialsValidation implements credentials {
      * @param {String} string - String on which to perform the validation.
      */
     Is_Password_with_Minimum_Eight_Characters_And_At_Least_One_Letter_And_One_Number(password: string): boolean {
-        const pattern: RegExp = new RegExp("^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$");
+        const pattern: RegExp = new RegExp(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/);
         const Password_Matches_Pattern: boolean = pattern.test(password);
         return Password_Matches_Pattern;
     }
@@ -35,7 +35,7 @@ export class CredentialsValidation implements credentials {
      * @param {String} string - String on which to perform the validation.
      */
     Is_Password_with_At_Least_Eight_Characters_A_Letter_A_Number_And_A_Special_Character(password: string): boolean{
-        const pattern: RegExp = new RegExp("^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$");
+        const pattern: RegExp = new RegExp(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/);
         const Password_Matches_Pattern: boolean = pattern.test(password);
         return Password_Matches_Pattern;
     }
@@ -45,7 +45,7 @@ export class CredentialsValidation implements credentials {
      * @param {String} string - String on which to perform the validation.
      */
     Is_Password_with_At_Least_Eight_Characters_An_Uppercase_Letter_A_Lowercase_Letter_One_Number(password: string): boolean{
-        const pattern: RegExp = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$");
+        const pattern: RegExp = new RegExp(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/);
         const Password_Matches_Pattern: boolean = pattern.test(password);
         return Password_Matches_Pattern;
     }
@@ -56,7 +56,7 @@ export class CredentialsValidation implements credentials {
      * @param {String} string - String on which to perform the validation.
      */
     Is_Password_with_At_Least_Eight_Characters_An_Uppercase_Letter_A_Lowercase_Letter_One_Number_A_Special_Character(password: string): boolean{
-        const pattern: RegExp = new RegExp("^(?=.*[\d])(?=.*[A-Z])(?=.*[a-z])(?=.*[!@#$%^&*])[\w!@#$%^&*]{8,}$");
+        const pattern: RegExp = new RegExp(/^(?=.*[\d])(?=.*[A-Z])(?=.*[a-z])(?=.*[!@#$%^&*])[\w!@#$%^&*]{8,}$/);
         const Password_Matches_Pattern: boolean = pattern.test(password);
         return Password_Matches_Pattern;
     }
