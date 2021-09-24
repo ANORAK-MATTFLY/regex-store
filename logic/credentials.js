73889389052543200b8e97870f6c3d0ac3513a5c
Ben Matanda <benmatanda354@gmail.com>
2021-09-24 07:56:46 +0100

    new release

--- logic/credentials.js
+++ logic/credentials.js
@@ -4,30 +4,35 @@ exports.CredentialsValidation = void 0;
 var CredentialsValidation = /** @class */ (function () {
     function CredentialsValidation() {
     }
+    CredentialsValidation.prototype.Is_Name = function (name) {
+        var pattern = new RegExp("^[a-zA-Z ]{2,30}$");
+        var Name_Matches_Pattern = pattern.test(name);
+        return Name_Matches_Pattern;
+    };
     CredentialsValidation.prototype.Is_Email = function (email) {
         var pattern = new RegExp("^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$");
-        var validationCheck = pattern.test(email);
-        return validationCheck;
+        var Email_Matches_Pattern = pattern.test(email);
+        return Email_Matches_Pattern;
     };
     CredentialsValidation.prototype.Is_Password_with_Minimum_Eight_Characters_And_At_Least_One_Letter_And_One_Number = function (password) {
         var pattern = new RegExp("^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$");
-        var Password_Match_Pattern = pattern.test(password);
-        return Password_Match_Pattern;
+        var Password_Matches_Pattern = pattern.test(password);
+        return Password_Matches_Pattern;
     };
     CredentialsValidation.prototype.Is_Password_with_At_Least_Eight_Characters_A_Letter_A_Number_And_A_Special_Character = function (password) {
         var pattern = new RegExp("^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$");
-        var Password_Match_Pattern = pattern.test(password);
-        return Password_Match_Pattern;
+        var Password_Matches_Pattern = pattern.test(password);
+        return Password_Matches_Pattern;
     };
     CredentialsValidation.prototype.Is_Password_with_At_Least_Eight_Characters_An_Uppercase_Letter_A_Lowercase_Letter_One_Number = function (password) {
         var pattern = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$");
-        var Password_Match_Pattern = pattern.test(password);
-        return Password_Match_Pattern;
+        var Password_Matches_Pattern = pattern.test(password);
+        return Password_Matches_Pattern;
     };
     CredentialsValidation.prototype.Is_Password_with_At_Least_Eight_Characters_An_Uppercase_Letter_A_Lowercase_Letter_One_Number_A_Special_Character = function (password) {
         var pattern = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$");
-        var Password_Match_Pattern = pattern.test(password);
-        return Password_Match_Pattern;
+        var Password_Matches_Pattern = pattern.test(password);
+        return Password_Matches_Pattern;
     };
     return CredentialsValidation;
 }());
--- regex.js
+++ regex.js
@@ -2,10 +2,6 @@
 exports.__esModule = true;
 exports.RegexStore = void 0;
 var credentials_1 = require("./logic/credentials");
-var RegexStore = /** @class */ (function () {
-    function RegexStore() {
-    }
-    RegexStore.credentials = new credentials_1.CredentialsValidation();
-    return RegexStore;
-}());
-exports.RegexStore = RegexStore;
+exports.RegexStore = Object.freeze({
+    credentials: new credentials_1.CredentialsValidation()
+});
